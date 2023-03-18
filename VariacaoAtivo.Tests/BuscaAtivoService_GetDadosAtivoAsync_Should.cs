f8fee9f8dfe6e9d63c6ee16af11e4e9871ce9672
Ondei <Ondei@ONDEI-PC>
2023-03-18 06:14:19 -0300

    testes unitarios

---
+++ VariacaoAtivo.Tests/BuscaAtivoService_GetDadosAtivoAsync_Should.cs
@@ -0,0 +1,18 @@
+using System;
+using VariacaoAtivo.Service;
+using VariacaoAtivo.Service.Interfaces;
+using Xunit;
+
+namespace VariacaoAtivo.Tests
+{
+    public class BuscaAtivoService_GetDadosAtivoAsync_Should
+    {
+        [Fact]
+        public void GetDadosAtivoAsync_InputIsCorrect_ReturnAListWithItems()
+        {
+            IBuscaAtivoService buscaAtivoService = new BuscaAtivoService();
+
+            //buscaAtivoService.
+        }
+    }
+}
